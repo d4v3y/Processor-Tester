d29925c4df80cbfdc3070e1f77642bfc8be8f1de
Davey Anguiano <daveya@Daveys-MacBook-Pro.local>
2019-09-17 12:39:43 -0700

    Finished C programs

--- CPU-bound.c
+++ CPU-bound.c
@@ -3,5 +3,20 @@
 
 int main() {
 
+    int totalT;
+    long increment = 0;
+    time_t secs = 60;
+    time_t startT = time(NULL);
+
+    printf("The fuction has started.\n");
+
+    while ((totalT = time(NULL) - startT) < secs){
+        increment++;
+    }
+
+    printf("The function has stopped.\n");
+    // printf("The function took %d seconds and ", totalT);
+    // printf("iterated %ld times.\n", increment);
+
     return 0;
 }
---
+++ IO-bound.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+
+    int totalT;
+    FILE *fptr;
+    char str[] = "I am writing to the file!\n";
+
+    fptr = fopen("randomText.txt", "w");
+    
+    time_t secs = 300;
+    time_t startT = time(NULL);
+
+    printf("The function has started.\n");
+
+    while ((totalT = time(NULL) - startT) < secs) {
+        for (int i = 0 ; str[i] != '\0'; i++) {
+            fputc(str[i], fptr);
+        }
+    }
+
+    fclose(fptr);
+
+    return 0;
+}
